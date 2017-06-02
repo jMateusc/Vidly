410f8d1e293654c90505d6d460ccec8e227922dc
Joao Mateus <jmateuscunha@gmail.com>
2017-06-02 16:36:57 -0300

    -Description

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Vidly.Controllers
 {
@@ -17,7 +13,7 @@ namespace Vidly.Controllers
         [AllowAnonymous]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Descrição da página";
 
             return View();
         }
